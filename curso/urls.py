b199eced2bd789bdb141739c4cf00a282bbec3ac
cristian <tahayk3@gmail.com>
2021-11-29 11:57:39 -0600

    terminado

--- curso/urls.py
+++ curso/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('curso_lista/lista', views.Curso, name ='curso_lista'),
+    url('curso_lista/lista', views.curso_nueva, name ='curso_lista'),
     url('', views.curso_nueva, name='curso_nueva'),
     ]
 
--- curso/views.py
+++ curso/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .forms import MateriaForm
-from curso.models import Alumno, Cursa, Materia
+from curso.models import Cursa, Materia
 
 def curso_nueva(request):
     if request.method == "POST":
         formulario = MateriaForm(request.POST)
         if formulario.is_valid():
-            materia = Materia.objects.create(nombre=formulario.cleaned_data['nombre'], anio = formulario.cleaned_data['anio'])
-            for alumno_id in request.POST.getlist('actores'):
-                actuacion = Cursa(alumno_id=alumno_id, Materia_id = materia.id)
+            pelicula = Materia.objects.create(nombre=formulario.cleaned_data['nombre'], anio = formulario.cleaned_data['anio'])
+            for actor_id in request.POST.getlist('actores'):
+                actuacion = Cursa(actor_id=actor_id, pelicula_id = pelicula.id)
                 actuacion.save()
-            messages.add_message(request, messages.SUCCESS, 'Curso Guardada Exitosamente')
+            messages.add_message(request, messages.SUCCESS, 'Pelicula Guardada Exitosamente')
     else:
         formulario = MateriaForm()
-    return render(request, 'materia/materia_editar.html', {'formulario': formulario})
+    return render(request, 'curso/curso_nueva.html', {'formulario': formulario})
