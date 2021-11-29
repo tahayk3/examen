a93d3419bc6a31949c2bf42b87ac7779b457bbcd
cristian <tahayk3@gmail.com>
2021-11-29 11:18:00 -0600

    cracion del modelo

--- curso/models.py
+++ curso/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Alumno(models.Model):
+    nombre  =   models.CharField(max_length=30)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+class Materia(models.Model):
+    nombre    = models.CharField(max_length=60)
+    anio      = models.IntegerField()
+    actores   = models.ManyToManyField(Alumno, through='Cursa')
+
+    def __str__(self):
+        return self.nombre
+
+class Cursa(models.Model):
+    actor = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    pelicula = models.ForeignKey(Materia, on_delete=models.CASCADE)
+
+class CursaInLine(admin.TabularInline):
+    model = Cursa
+    extra = 1
+
+
+class AlumnoAdmin(admin.ModelAdmin):
+    inlines = (CursaInLine,)
+
+class MateriaAdmin (admin.ModelAdmin):
+    inlines = (CursaInLine,)
